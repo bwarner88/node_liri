f6ac22decb0d030be808f92d69b74549ad3d12d1
unknown <brian_warner25@yahoo.com>
2018-07-02 07:53:45 -0700

    tweets

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: "tEnOEIGXcFS6WJL47RopYe9c8",
+  consumer_secret: "l9Dwzbw5fwPHOXiQAFxol4reC5SaW2SdVWuZgbIFriDGi4ZJ1R",
+  access_token_key: "1011780758594207745 - SPoJVhxdc4O9Gy5bbdSKRRdO804SM0",
+  access_token_secret: "xzSWuzz7LA9EAMVVYM0ssZO7G8mA6WjNysWmp8swTnoTY"
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "02c5f8d7e17d489bb76fc8ef5d1a526b",
+  secret: "5d0100a66b7b45cd9fc6ec70fe076c5d"
 };
--- liri.js
+++ liri.js
@@ -1,9 +1,42 @@
-require("dotenv").config();
+require("./.env").config();
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
+
+var keys = require('./keys.js');
+var twitter = require("twitter");
+var spotifyReq = require("spotify");
+var axios = require("axio");
+var fs = require('fs');
 
 var term = process.argv[2]
 var search = process.argv.slice(3).join(" ");
+
+
+var getTweet = function () {
+    var client = new Twitter(keys.twitter);
+
+    var userName = {
+        screen_name: "BrianWarner",
+        count: 20
+    }
+
+    client.get('statuses/user_timeline', userName, function (error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++) {
+                var returnedData = ('Number: ' + (i + 1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+                console.log(returnedData);
+            }
+        }
+    });
+
+
+}
+
+var argParams = function(){
+    if(term === 'my-tweets'){
+        getTweet();
+    }
+}
+argParams();
 
